c94ad9cda76692160e233a38797ce3ddb65e1b84
Clément Stenac <clement.stenac@exalead.com>
2012-10-05 13:43:28 +0200

    Cleanup and add parameters

--- src/fr/openstreetmap/search/autocomplete/AutocompletionServlet.java
+++ src/fr/openstreetmap/search/autocomplete/AutocompletionServlet.java
@@ -26,68 +26,77 @@ import fr.openstreetmap.search.autocomplete.MultipleWordsAutocompleter.MultiWord
 import fr.openstreetmap.search.autocomplete.OSMAutocompleteUtils.MatchData;
 
 public class AutocompletionServlet extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-	MultipleWordsAutocompleter mwa;
-	Set<String> stopWords = new HashSet<String>();
+    private static final long serialVersionUID = 1L;
+    MultipleWordsAutocompleter mwa;
+    Set<String> stopWords = new HashSet<String>();
 
-	public void initSW(String swFile) throws Exception {
-		BufferedReader br = new BufferedReader(new FileReader(swFile));
-		while (true) {
-			String line = br.readLine();
-			if (line == null) break;
-			stopWords.add(line.replace("\n", ""));
-		}
-	}
-	
-	static class FinalResult {
-	    public FinalResult(MultiWordAutocompleterEntry ae) {
-	        this.ae = ae;
-	    }
-	    MultiWordAutocompleterEntry ae;
-	    byte[] encodedData;
-	    MatchData decodedData;
-	}
+    public void initSW(String swFile) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(swFile));
+        while (true) {
+            String line = br.readLine();
+            if (line == null) break;
+            stopWords.add(line.replace("\n", ""));
+        }
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		long startup = System.nanoTime();
-		String query = req.getParameter("q");
-		if (query == null) {
-			query = req.getParameter("term");
-		}
-		System.out.println("QUERY: " + query);
+    static class FinalResult {
+        public FinalResult(MultiWordAutocompleterEntry ae) {
+            this.ae = ae;
+        }
+        MultiWordAutocompleterEntry ae;
+        byte[] encodedData;
+        MatchData decodedData;
+    }
 
-		List<String> stopped = new ArrayList<String>();
-		List<String> tokensList = new ArrayList<String>();
-		CreateFromDBOutput.tokenize(query, tokensList);
+    int getIntParam(HttpServletRequest req, String name, int defaultValue) {
+        String v = req.getParameter(name);
+        if (v == null) return defaultValue;
+        return Integer.parseInt(v);
+    }
 
-		ListIterator<String> it = tokensList.listIterator();
-		while (it.hasNext()) {
-			String token = it.next();
-			if (stopWords.contains(token)) {
-				stopped.add(token);
-				it.remove();
-			}
-			if (token.length() < 3) {
-				stopped.add(token);
-				it.remove();
-			}
-			if (CreateFromDBOutput.definitelyStopWords.contains(token)) {
-				stopped.add(token);
-				it.remove();
-			}
-		}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-		synchronized (this) {
-			MultipleWordsAutocompleter.DebugInfo di = new MultipleWordsAutocompleter.DebugInfo();
+        int returnLimit = getIntParam(req, "limit", 25);
+        int boostLimit = getIntParam(req, "boostLimit", 200);
+        int debug = getIntParam(req, "debug", 1);
 
-			long beforeQuery= System.nanoTime();
-			List<MultiWordAutocompleterEntry> results = mwa.autocomplete(tokensList, 1, di);
-			long afterQuery= System.nanoTime();
+        long startup = System.nanoTime();
+        String query = req.getParameter("q");
+
+        System.out.println("QUERY: " + query);
+
+        List<String> stopped = new ArrayList<String>();
+        List<String> tokensList = new ArrayList<String>();
+        CreateFromDBOutput.tokenize(query, tokensList);
+
+        ListIterator<String> it = tokensList.listIterator();
+        while (it.hasNext()) {
+            String token = it.next();
+            if (stopWords.contains(token)) {
+                stopped.add(token);
+                it.remove();
+            }
+            if (token.length() < 3) {
+                stopped.add(token);
+                it.remove();
+            }
+            if (CreateFromDBOutput.definitelyStopWords.contains(token)) {
+                stopped.add(token);
+                it.remove();
+            }
+        }
+
+        synchronized (this) {
+            MultipleWordsAutocompleter.DebugInfo di = new MultipleWordsAutocompleter.DebugInfo();
+
+            long beforeQuery= System.nanoTime();
+            List<MultiWordAutocompleterEntry> results = mwa.autocomplete(tokensList, 1, di);
+            long afterQuery= System.nanoTime();
 
             /* First, sort by distance then score */
-			Collections.sort(results, new Comparator<MultiWordAutocompleterEntry>() {
+            Collections.sort(results, new Comparator<MultiWordAutocompleterEntry>() {
                 @Override
                 public int compare(MultiWordAutocompleterEntry o1, MultiWordAutocompleterEntry o2) {
                     if (o1.distance < o2.distance) return -1;
@@ -97,110 +106,113 @@ public class AutocompletionServlet extends HttpServlet{
                     return 0;
                 }
             });
-			
-			/* If there are some stopped words, but we can find them in the summary, then strongly boost the score.
-			 * This way, stuff that did not really match will get downvoted.
-			 */
-			List<FinalResult> fresults = new ArrayList<AutocompletionServlet.FinalResult>();
+
+            long afterSort1 = System.nanoTime();
+
+            /* If there are some stopped words, but we can find them in the summary, then strongly boost the score.
+             * This way, stuff that did not really match will get downvoted.
+             */
+            List<FinalResult> fresults = new ArrayList<AutocompletionServlet.FinalResult>();
             OSMAutocompleteUtils utils = new OSMAutocompleteUtils();
 
-			int nbRes = 0;
-			for (MultiWordAutocompleterEntry ae : results) {
-			    FinalResult fr = new FinalResult(ae);
-			    fr.encodedData = mwa.completer.getByteData(ae.offset);
-			    
-			    fr.decodedData = utils.decodeData(fr.encodedData);
-			    
-			    for (String stop : stopped) {
-			        if (fr.decodedData.name.toLowerCase().contains(stop)) {
-			            System.out.println("  Boosting " + fr.decodedData);
-			            fr.ae.score += 1000;
-			        }
-			    }
-			    fresults.add(fr);
-			    
-			    // Don't decode too much
-			    if (nbRes++ > 100) break;
-			}
-			Collections.sort(fresults, new Comparator<FinalResult>() {
+            int nbRes = 0;
+            for (MultiWordAutocompleterEntry ae : results) {
+                FinalResult fr = new FinalResult(ae);
+                fr.encodedData = mwa.completer.getByteData(ae.offset);
 
+                fr.decodedData = utils.decodeData(fr.encodedData);
+
+                for (String stop : stopped) {
+                    if (fr.decodedData.name.toLowerCase().contains(stop)) {
+                        //			            System.out.println("  Boosting " + fr.decodedData);
+                        fr.ae.score += 1000;
+                    }
+                }
+                fresults.add(fr);
+
+                // Don't decode too much
+                if (nbRes++ > boostLimit) break;
+            }
+
+            long afterBoost = System.nanoTime();
+
+            Collections.sort(fresults, new Comparator<FinalResult>() {
                 @Override
                 public int compare(FinalResult o1, FinalResult o2) {
                     return o1.ae.compareTo(o2.ae);
                 }
             });
-			
-			long afterSort= System.nanoTime();
 
-			resp.setContentType("application/json");
+            long afterSort= System.nanoTime();
 
-			BufferedWriter bwr = new BufferedWriter(resp.getWriter());
-			JSONWriter wr = new JSONWriter(bwr);
-			try {
-				wr.object().key("matches").array();
+            resp.setContentType("application/json");
 
-				nbRes = 0;
-				for (FinalResult fr : fresults) {
-//					System.out.println("   RES  " + fr.decodedData + " d=" + fr.ae.distance + " s=" + fr.ae.score + " p=" + fr.ae.correctedTokens);
-					//					System.out.println(" " + ae.offset + " - " + ae.score + " " + ae.distance + " correct prefix=" + ae.correctedPrefix);
-					//        	System.out.println("   " + a.getData(ae.offset));
-					wr.object();
-					wr.key("name").value(fr.decodedData.name);
-					wr.key("lat").value(fr.decodedData.lat);
-					wr.key("lon").value(fr.decodedData.lon);
-					wr.key("type").value(fr.decodedData.type);
-					wr.key("cities").value(StringUtils.join(fr.decodedData.cityNames, ", "));
-					wr.key("distance").value(fr.ae.distance);
-					wr.key("score").value(fr.ae.score);
-					wr.key("prefix").value(StringUtils.join(fr.ae.correctedTokens, " "));
-					wr.endObject();
-					if (nbRes++ > 25) break;
-				}
-				wr.endArray();
+            BufferedWriter bwr = new BufferedWriter(resp.getWriter());
+            JSONWriter wr = new JSONWriter(bwr);
+            try {
+                wr.object().key("matches").array();
 
-				long afterWriteData = System.nanoTime();
+                nbRes = 0;
+                for (FinalResult fr : fresults) {
+                    wr.object();
+                    wr.key("name").value(fr.decodedData.name);
+                    wr.key("lat").value(fr.decodedData.lat);
+                    wr.key("lon").value(fr.decodedData.lon);
+                    wr.key("type").value(fr.decodedData.type);
+                    wr.key("cities").value(StringUtils.join(fr.decodedData.cityNames, ", "));
+                    wr.key("distance").value(fr.ae.distance);
+                    wr.key("score").value(fr.ae.score);
+                    wr.key("prefix").value(StringUtils.join(fr.ae.correctedTokens, " "));
+                    wr.endObject();
+                    if (nbRes++ > returnLimit) break;
+                }
+                wr.endArray();
 
-				wr.key("debug").object();
-				wr.key("stopWords").value(StringUtils.join(stopped, ", "));
-				wr.key("tokens").array();
-				for (int i = 0; i < di.tokensDebugInfo.size(); i++) {
-					wr.object().key("token").value(di.tokensDebugInfo.get(i).value);
-					wr.key("rtMatches").value(di.tokensDebugInfo.get(i).radixTreeMatches);
-					wr.key("decodedMatches").value(di.tokensDebugInfo.get(i).decodedMatches);
-					wr.key("rtMatchTime").value(di.tokensDebugInfo.get(i).radixTreeMatchTime);
-					wr.key("decodingTime").value(di.tokensDebugInfo.get(i).listsDecodingTime);
-					wr.endObject();
-				}
-				wr.endArray(); // tokens
-				wr.key("finalMatches").value(results.size());
-
-				long atEnd = System.nanoTime();
-
-				wr.key("preprocessTime").value((beforeQuery - startup)/1000);
-				wr.key("processTime").value((afterQuery - beforeQuery)/1000);
-                wr.key("totalMatchTime").value(di.totalTokensMatchTime);
-                wr.key("filterTime").value(di.filterTime);
-				wr.key("intersectionTime").value(di.intersectionTime);
-				wr.key("sortTime").value((afterSort - afterQuery)/1000);
-				wr.key("resultsWriteTime").value((afterWriteData- afterSort)/1000);
-				wr.key("totalServerTime").value((atEnd - startup)/1000);
-				wr.endObject();
-				wr.endObject();
+                long atEnd = System.nanoTime();
+                
+                if (debug > 0) {
+                    wr.key("debug").object();
+                    wr.key("stopWords").value(StringUtils.join(stopped, ", "));
+                    wr.key("tokens").array();
+                    for (int i = 0; i < di.tokensDebugInfo.size(); i++) {
+                        wr.object().key("token").value(di.tokensDebugInfo.get(i).value);
+                        wr.key("rtMatches").value(di.tokensDebugInfo.get(i).radixTreeMatches);
+                        wr.key("decodedMatches").value(di.tokensDebugInfo.get(i).decodedMatches);
+                        wr.key("rtMatchTime").value(di.tokensDebugInfo.get(i).radixTreeMatchTime);
+                        wr.key("decodingTime").value(di.tokensDebugInfo.get(i).listsDecodingTime);
+                        wr.endObject();
+                    }
+                    wr.endArray(); // tokens
+                    wr.key("finalMatches").value(results.size());
+                  
+                    wr.key("preprocessTime").value((beforeQuery - startup)/1000);
+                    wr.key("processTime").value((afterQuery - beforeQuery)/1000);
+                    wr.key("totalMatchTime").value(di.totalTokensMatchTime);
+                    wr.key("filterTime").value(di.filterTime);
+                    wr.key("intersectionTime").value(di.intersectionTime);
+                    wr.key("sortTime").value((afterSort - afterQuery)/1000);
+                    wr.key("resultsWriteTime").value((atEnd- afterSort)/1000);
+                    wr.key("totalServerTime").value((atEnd - startup)/1000);
+                    wr.endObject();
+                }
+                wr.endObject();
 
                 bwr.flush();
-                
-				System.out.print("QUERY: " + query + " DONE: totalMatches=" + results.size());
-				System.out.print(" totalTime=" + (atEnd-startup)/1000);
-				System.out.print(" preprocTime=" + (beforeQuery - startup)/1000);
-				System.out.print(" processTime=" + (afterQuery - beforeQuery)/1000);
-                System.out.print(" (match=" + di.totalTokensMatchTime + " filter=" + di.filterTime + " intersect=" + di.intersectionTime + ")");
-				System.out.print(" sortTime=" + (afterSort - afterQuery)/1000);
-				System.out.print(" writeTime=" + (afterWriteData- afterSort)/1000);
 
-			} catch (JSONException e) {
-				throw new IOException(e);
-			}
-		}
-	}
+                System.out.print("QUERY: " + query + " DONE: totalMatches=" + results.size());
+                System.out.print(" totalTime=" + (atEnd-startup)/1000);
+                System.out.print(" preprocTime=" + (beforeQuery - startup)/1000);
+                System.out.print(" processTime=" + (afterQuery - beforeQuery)/1000);
+                System.out.print(" (match=" + di.totalTokensMatchTime + " filter=" + di.filterTime + " intersect=" + di.intersectionTime + ")");
+                System.out.print(" sortTime=" + (afterSort - afterQuery)/1000 );
+                System.out.print(" (s1=" + (afterSort1 - afterQuery)/1000 + " b=" + (afterBoost-afterSort1)/1000 + " s2=" + (afterSort-afterBoost)/1000 + ")");
+
+                System.out.println(" writeTime=" + (atEnd- afterSort)/1000);
+
+            } catch (JSONException e) {
+                throw new IOException(e);
+            }
+        }
+    }
 
 }
